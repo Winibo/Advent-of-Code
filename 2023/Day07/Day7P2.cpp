1cb70388753d7091fc47fc910c65f77c5935127d
Winibo <liamnb@telus.net>
2023-12-06 22:41:10 -0700

    Add Day 7 2023

---
+++ 2023/Day07/Day7P2.cpp
@@ -0,0 +1,84 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+string cardRanks = "J23456789TQKA";
+
+int getScore(string hand) {
+    int longestMatch = 0, uniqueValues = 0, currentMatch = 0, jokers = 0;
+    for (int i = 0; i < hand.length(); i++) {
+        if (hand[i] == 'J') {
+            currentMatch = 0; //Don't count this in our list of matches
+            //Jokers pretend to be a different card, don't increment unique values
+            jokers++;
+            continue;
+        }
+        if (i == 0 || hand[i] != hand[i-1]) {
+            uniqueValues++;
+            longestMatch = max(longestMatch, currentMatch);
+            currentMatch = 1;
+        }
+        if (i == 0 || hand[i] == hand[i-1]) {
+            if (i != 0)
+                currentMatch++;
+            longestMatch = max(longestMatch, currentMatch);
+        }
+    }
+    longestMatch += jokers;
+    if (longestMatch > 2) {
+        if (uniqueValues <= 2) {
+            return longestMatch + 1; //6,5,4 
+        } else {
+            return 3; //3
+        }
+    } else if (uniqueValues == 3) {
+        return 2;
+    } else if (uniqueValues == 4) {
+        return 1;
+    }
+    return 0;
+}
+
+bool sortHands(pair<string, int> a, pair<string, int> b) {
+    string sortedA = a.first, sortedB = b.first;
+    sort(sortedA.begin(), sortedA.end());
+    sort(sortedB.begin(), sortedB.end());
+    int aScore = getScore(sortedA);
+    int bScore = getScore(sortedB);
+    if (aScore > bScore) {
+        return true;
+    } else if (aScore < bScore) {
+        return false;
+    } else {
+        //Break with first card
+        for (int i = 0; i < sortedA.length(); i++) {
+            int aPow = cardRanks.find(a.first[i]);
+            int bPow = cardRanks.find(b.first[i]);
+            if (aPow > bPow) {
+                return true;
+            } else if (aPow < bPow){
+                return false;
+            }
+        }
+    }
+    return true; //Unreachable
+}
+
+int main() {
+    vector<pair<string, int>> hands;
+    long long score = 0;
+    while (!cin.eof()) {
+        string hand;
+        int wager = -1;
+        cin >> hand >> wager;
+        if (wager == -1)
+            break;
+        hands.push_back(make_pair(hand, wager));
+    }
+    sort(hands.rbegin(), hands.rend(), sortHands);
+    for (int i = 0; i < hands.size(); i++) {
+        score += hands[i].second * (i+1);
+    }
+    cout << score << endl;
+    return 0;
+}
