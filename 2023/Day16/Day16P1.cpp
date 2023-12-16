f23ced23e9735e10f027ec3fe6daccda7e6f666e
Winibo <liamnb@telus.net>
2023-12-15 22:34:31 -0700

    Day 16 2023

---
+++ 2023/Day16/Day16P1.cpp
@@ -0,0 +1,104 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+vector<string> mapGrid;
+
+map<pair<int, int>, vector<int>> visited; //Row, Column order, stores direction of travel
+
+void followPath(pair<int, int> start, int direction) { //Row, Column order, 0,1,2,3 -> N,E,S,W (U,R,D,L)
+    //Repeat until we fall off the grid
+    while (start.first >= 0 && start.first < mapGrid.size() && start.second >= 0 && start.second < mapGrid[0].length()) {
+        for (int i : visited[start]) {
+            if (i == direction)
+                return; //Loop detection
+        }
+        visited[start].push_back(direction); //Add direction to list
+        if (mapGrid[start.first][start.second] == '/') {
+            switch (direction) {
+                case 0:
+                    direction = 1;
+                    break;
+                case 1:
+                    direction = 0;
+                    break;
+                case 2:
+                    direction = 3;
+                    break;
+                case 3:
+                    direction = 2;
+                    break;
+            }
+        } else if (mapGrid[start.first][start.second] == '\\') {
+            switch (direction) {
+                case 0:
+                    direction = 3;
+                    break;
+                case 1:
+                    direction = 2;
+                    break;
+                case 2:
+                    direction = 1;
+                    break;
+                case 3:
+                    direction = 0;
+                    break;
+            }
+        } else if (mapGrid[start.first][start.second] == '|') {
+            switch (direction) {
+                case 1:
+                case 3:
+                    start.first--;
+                    followPath(start, 0);
+                    start.first += 2;
+                    followPath(start, 2);
+                    return; //Nothing more to do here
+                case 0:
+                case 2:
+                    break;
+            }
+        } else if ((mapGrid[start.first][start.second] == '-')) {
+            switch (direction) {
+                case 0:
+                case 2:
+                    start.second--;
+                    followPath(start, 3);
+                    start.second += 2;
+                    followPath(start, 1);
+                    return; //Nothing more to do here
+                case 1:
+                case 3:
+                    break;
+            }
+        }
+        
+        switch (direction) {
+            case 0:
+                start.first--;
+                break;
+            case 1:
+                start.second++;
+                break;
+            case 2:
+                start.first++;
+                break;
+            case 3:
+                start.second--;
+                break;
+        }
+    }
+}
+
+
+int main() {
+    while (!cin.eof()) {
+        string next = "";
+        getline(cin, next);
+        if (next == "")
+            break;
+        mapGrid.push_back(next);
+    }
+    followPath(make_pair(0, 0), 1);
+    cout << visited.size() << endl;
+    return 0;
+}
